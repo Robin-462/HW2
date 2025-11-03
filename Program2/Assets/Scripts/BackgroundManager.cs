62a1db96c633033c52a4224efe1be7b5910d0c18
Robin-462 <ruochenzheng4@gmail.com>
2025-11-02 21:19:56 -0700

    7

---
+++ Program2/Assets/Scripts/BackgroundManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class BackgroundManager : MonoBehaviour
+{
+    public List<GameObject> layer10Sky = new List<GameObject>();
+    public List<GameObject> layer09Forest = new List<GameObject>();
+    public List<GameObject> layer08Forest = new List<GameObject>();
+    public List<GameObject> layer07Forest = new List<GameObject>();
+    public List<GameObject> layer06Forest = new List<GameObject>();
+    public List<GameObject> layer05Particle = new List<GameObject>();
+    public List<GameObject> layer04Forest = new List<GameObject>();
+    public List<GameObject> layer03Particle = new List<GameObject>();
+    public List<GameObject> layer02Bushes = new List<GameObject>();
+    public List<GameObject> layer01Mist = new List<GameObject>();
+}
--- Program2/Assets/Scripts/ParallaxScroller.cs
+++ Program2/Assets/Scripts/ParallaxScroller.cs
@@ -3,57 +3,103 @@
 // Sample code file
 
 using UnityEngine;
+using System.Collections.Generic;
 
 public class ParallaxScroller : MonoBehaviour
 {
-    public GameObject parallaxCamera;
     public float parallaxLevel = 0.5f;
     public float playerMoveSpeed = 5f;
+    public BackgroundManager backgroundManager;
 
-    private float startPos;
     private float layerWidth;
     private Camera cam;
+    private GameObject currentBackground;
 
     void Start()
     {
-        startPos = transform.position.x;
-
+        cam = Camera.main;
         SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        if (sr != null)
+        {
+            layerWidth = sr.bounds.size.x;
+        }
+        MakeRandomBackground();
     }
 
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
+        float input = Input.GetAxis("Horizontal");
+        if (input == 0) return;
         
-        if (Mathf.Abs(horizontalInput) < 0.1f) 
-            return;
+        float move = -input * playerMoveSpeed * Time.deltaTime * parallaxLevel;
+        Vector3 newPos = transform.position + new Vector3(move, 0, 0);
         
-        float scrollDistance = -horizontalInput * playerMoveSpeed * Time.deltaTime * parallaxLevel;
-        Vector3 newPos = transform.position + new Vector3(scrollDistance, 0, 0);
-        
-        MakeBackgroundRepeat(ref newPos);
-        
+        CheckLoop(ref newPos);
         transform.position = newPos;
     }
 
-    void MakeBackgroundRepeat(ref Vector3 position)
+    void CheckLoop(ref Vector3 pos)
     {
         if (cam == null) return;
 
-        float camHalfWidth = cam.orthographicSize * cam.aspect;
-        float camRightEdge = cam.transform.position.x + camHalfWidth;
-        float camLeftEdge = cam.transform.position.x - camHalfWidth;
+        float camWidth = cam.orthographicSize * cam.aspect;
+        float camLeft = cam.transform.position.x - camWidth;
+        float camRight = cam.transform.position.x + camWidth;
 
-        float layerRightEdge = position.x + layerWidth / 2;
-        float layerLeftEdge = position.x - layerWidth / 2;
+        float myLeft = pos.x - layerWidth / 2;
+        float myRight = pos.x + layerWidth / 2;
 
-        if (layerRightEdge < camLeftEdge)
+        if (myRight < camLeft)
         {
-            position.x += layerWidth;
+            pos.x += layerWidth;
+            MakeRandomBackground();
         }
-        else if (layerLeftEdge > camRightEdge)
+        else if (myLeft > camRight)
         {
-            position.x -= layerWidth;
+            pos.x -= layerWidth;
+            MakeRandomBackground();
         }
+    }
+
+    void MakeRandomBackground()
+    {
+        if (backgroundManager == null) return;
+        
+        List<GameObject> variants = GetVariants();
+        if (variants == null || variants.Count == 0) return;
+
+        int randomIndex = Random.Range(0, variants.Count);
+        GameObject variant = variants[randomIndex];
+        
+        if (variant != null)
+        {
+            if (currentBackground != null)
+            {
+                Destroy(currentBackground);
+            }
+            
+            currentBackground = Instantiate(variant, transform);
+            currentBackground.transform.localPosition = Vector3.zero;
+        }
+    }
+
+    List<GameObject> GetVariants()
+    {
+        if (backgroundManager == null) return null;
+        
+        string objName = gameObject.name.ToLower();
+        
+        if (objName.Contains("10")) return backgroundManager.layer10Sky;
+        if (objName.Contains("09")) return backgroundManager.layer09Forest;
+        if (objName.Contains("08")) return backgroundManager.layer08Forest;
+        if (objName.Contains("07")) return backgroundManager.layer07Forest;
+        if (objName.Contains("06")) return backgroundManager.layer06Forest;
+        if (objName.Contains("05")) return backgroundManager.layer05Particle;
+        if (objName.Contains("04")) return backgroundManager.layer04Forest;
+        if (objName.Contains("03")) return backgroundManager.layer03Particle;
+        if (objName.Contains("02")) return backgroundManager.layer02Bushes;
+        if (objName.Contains("01")) return backgroundManager.layer01Mist;
+        
+        return null;
     }
 }
